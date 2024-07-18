2dd1f828a7f7b5e6d600497da484450b6ab65da6
momesso <momessoalves@gmail.com>
2024-07-18 09:50:18 -0300

    Bug do movimento periodico corrigido

--- snake_code/install/serial_ctrl/share/serial_ctrl/serial_ctrl_py.py
+++ snake_code/install/serial_ctrl/share/serial_ctrl/serial_ctrl_py.py
@@ -4,7 +4,7 @@ from sensor_msgs.msg import JointState
 from math import pi, floor
 import serial
 import threading
-import keyboard  # Import the keyboard library
+
 
 
 class MinimalSubscriber(Node):
@@ -16,19 +16,40 @@ class MinimalSubscriber(Node):
         self.ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
         self.ser.setRTS(False)
         self.ser.setDTR(False)
-       
-        self.create_timer(0.2, self.read_message)
+
+     
+        self.set_position_periodic()
+        self.read_message_periodic()
         
-        stop_message = [
-            0x55, 0x55, 0x02, 0x07,
-        ]
-        self.ser.write(stop_message)
-
         self.message1 = 0
+    
+    
+    def read_message(self):
+        message3 = [
+            0x55, 0x55, 0x08, 0x15,
+            0x05, 0x01, 0x02, 0x03,
+            0x04, 0x05
+        ]
 
-    def set_position(self):
-        array = self.position
-        msgs = [0] * (5 * 2)
+        #PARAR O ROBO
+        message3 = [
+            0x55, 0x55, 0x02, 0x07,
+            
+        ]
+
+
+        self.ser.write(message3)
+        data = self.ser.read(20)
+
+        print("data:", data)
+
+    def read_message_periodic(self):
+        threading.Timer(0.75, self.read_message_periodic).start()
+        self.read_message()
+       
+
+    def set_position(self, array, timer=False):
+        msgs = [0] * (5 * 2)  
 
         if len(array) != 5:
             self.get_logger().warn(f"Posição do robô não publicada")
@@ -38,6 +59,8 @@ class MinimalSubscriber(Node):
                 numero = floor(array[i])
                 msgs[2 * i] = numero & 0x000000ff
                 msgs[2 * i + 1] = (numero & 0x0000ff00) >> 8
+
+           
 
             message = [
                 0x55, 0x55, 0x14, 0x03, 0x05, 0x20, 0x03, 0x01,
@@ -51,37 +74,24 @@ class MinimalSubscriber(Node):
             if self.message1 != message:
                 self.ser.write(message)
                 print("Mensagem enviada! Mensagem:", message)
-                print("Data:", bytearray(message))
                 self.message1 = message
             else:
                 self.message1 = message
 
-            # if not timer:
-            #     print("Posição:", array)
+
+            # print("Mensagem1:", self.message1)
+            # print("Mensgem:", message)
+
+            if not timer:
+                print("Posição:", array)
 
        
-    def read_message(self):
         
-        message3 = [
-            0x55, 0x55, 0x08, 0x15,
-            0x05, 0x01, 0x02, 0x03,
-            0x04, 0x05
-        ]
-        
-        self.ser.write(message3)
-        data = self.ser.read(20)
-        message = data.hex()
-        separated_string = ' '.join([message[i:i+2] for i in range(0, len(message), 2)])
-        print("Data received:", separated_string)
-
        
-    # def set_position_periodic(self):
-    #     threading.Timer(0.1, self.set_position_periodic).start()
-    #     self.set_position(self.position, timer=True)
-
-    # def read_message_periodic(self):
-    #     threading.Timer(0.01, self.read_message_periodic).start()
-    #     self.read_message()
+    def set_position_periodic(self):
+        threading.Timer(0.1, self.set_position_periodic).start()  
+        self.set_position(self.position, timer=True)
+       
 
     def listener_callback(self, msg):
         a = msg.position
@@ -92,20 +102,16 @@ class MinimalSubscriber(Node):
         angle5 = a[4] * (180.0 / pi)
 
         array = [angle1, angle2, angle3, angle4, angle5]
-        self.position = array
-
-        self.set_position()
-
-   
+        self.position = array   
 
 def main(args=None):
+    
     rclpy.init(args=args)
     minimal_subscriber = MinimalSubscriber()
     rclpy.spin(minimal_subscriber)
 
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
